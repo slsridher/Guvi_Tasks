a179292e9ae6c556e405427aff63f3a646a47c2e
slsridher <80046660+slsridher@users.noreply.github.com>
2021-03-23 08:54:41 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+var request=new XMLHttpRequest(); 
+request.open('GET','https://restcountries.eu/rest/v2/all',true);
+request.send();
+request.onload=function(){
+    var data=JSON.parse(this.response)
+    for (var i=0;i<data.length;i++)
+    {
+        console.log(data[i].name)
+    }
+}
